4673e7fc66b5ca6876a400c9086e7cf7d5839f71
liuyc1515 <liuyicheng1515@sjtu.edu.cn>
2023-04-28 22:40:36 +0800

    another try

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 
 // ReactDOM.render(<App/>, document.getElementById('root'));
 ReactDOM.render((
-    <BrowserRouter>
+    <BrowserRouter basename="yichengliu.github.io">
       <App />
     </BrowserRouter>
  ), document.getElementById('root'));
