19a852c1314359202650f74d07f18ca8894db5f4
liuyc1515 <liuyicheng1515@sjtu.edu.cn>
2023-04-28 22:51:36 +0800

    another try

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import BasicRouter from './router/Router';
 
 function App() {
   return (
-    <Router basename='/'>
+    <Router basename='/yichengliu.github.io'>
       <BasicRouter/>
     </Router>
   );
