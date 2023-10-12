68983f4c4f336e3b6c93abcb8334c37207a173eb
liuyc1515 <liuyicheng1515@sjtu.edu.cn>
2023-10-12 23:31:03 +0800

    test format authors v6

--- src/components/Publication.js
+++ src/components/Publication.js
@@ -12,24 +12,22 @@ class Conference extends React.Component {
                             <div>
                                 <div className="paper"><a href={paper.paperLink}>{paper.title}</a></div>
                                 <div className="authors">
-                                    <p>
-                                        {
-                                            paper.authors.map((author) => {
-                                                if (author.includes("Yicheng Liu"))
-                                                {
-                                                    return (
-                                                        <box className="myself">{author};&nbsp;</box>
-                                                    )
-                                                }
-                                                else
-                                                {
-                                                    return (
-                                                        <box>{author};&nbsp;</box> 
-                                                    )
-                                                }
-                                            })
-                                        }
-                                    </p>
+                                    {
+                                        paper.authors.map((author) => {
+                                            if (author.includes("Yicheng Liu"))
+                                            {
+                                                return (
+                                                    <box className="myself">{author};&nbsp;</box>
+                                                )
+                                            }
+                                            else
+                                            {
+                                                return (
+                                                    <box>{author};&nbsp;</box> 
+                                                )
+                                            }
+                                        })
+                                    }
                                 </div>
                                 <div className="conference">{paper.conference}</div>
                                 <div className="paper">
