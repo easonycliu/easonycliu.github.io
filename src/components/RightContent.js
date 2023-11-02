78b7e58aff6bf2971f29d32f29ed20a7dee0849f
liuyc1515 <liuyicheng1515@sjtu.edu.cn>
2023-11-02 23:03:54 +0800

    update cv

--- src/components/RightContent.js
+++ src/components/RightContent.js
@@ -22,8 +22,7 @@ export default class RightContent extends React.Component {
                     <h1>Yicheng Liu</h1>
                     <p>I am now an undergraduate student at Shanghai Jiao Tong University (SJTU).</p>
                     <p>
-                        I have been working as a research intern at John Hopcoft Center since 2021, advised by Guanjie Zheng. 
-                        I am now a student at Institution of Parallel And Distributed System (IPADS), advised by Jinyu Gu.
+                        I'm an intern at Institution of Parallel And Distributed System (IPADS), advised by Jinyu Gu and Haibo Chen.
                         In this summer, I visited and took part in the research in University of Michigan, OrderLab, advised by Ryan Huang and mentored by Yigong Hu.
                     </p>
                     <div className="link">
@@ -31,7 +30,27 @@ export default class RightContent extends React.Component {
                             <a href="mailto:liuyicheng1515@sjtu.edu.cn">Email</a> / <a href={cv}>CV</a> / <a href="https://github.com/liuyc1515/">GitHub</a>
                         </p>
                     </div>
-                    
+                </div>
+
+                <div id="Research Interests">
+                    <h2>Research Interests</h2>
+                    <p>
+                        <b>Make system secure and reliable: </b>Resource contention is awkward. 
+                        Some abnormal workloads leading the system to severe contention depresses the reliability.
+                        Other malicious users making use of the contention to launch DoS attack harms the security.
+                        I work toward analyzing and mitigating the contention with some novel mechanisms.
+                    </p>
+
+                    <p>
+                        <b>Make secure and reliable system faster: </b>Security is expensive.
+                        The software solution costs efficiency.
+                        The hardware solution costs money.
+                        I seek a co-design approach to accelerate current solutions while making minimal changes to existing hardware.
+                    </p>
+
+                    <p>
+                        <b>AI 4 SYS: </b>I have experience in AI research and am eager to combine this expertise with system research.
+                    </p>
                 </div>
 
                 <div id="Research Experiences">
@@ -41,40 +60,10 @@ export default class RightContent extends React.Component {
 
                 <div id="Publications">
                     <h2>Publications</h2>
-                    <h4>Conference</h4>
+                    {/* <h4>Conference</h4> */}
                     <Conference />
                     {/* <h4>arXiv</h4> */}
                     <Arxiv />
-                </div>
-
-                <div id="Research Interests">
-                    <h2>Research Interests</h2>
-                    <p>
-                        Currently, my focus is on system security, with a specific emphasis on virtual machine (VM) and container security. 
-                        In the realm of security, there exist three primary objectives: Confidentiality, Integrity, and Availability (CIA). 
-                        While the majority of research in this field has concentrated on maintaining confidentiality and integrity, 
-                        I am dedicated to extending the boundaries of defending against availability-based attacks while concurrently upholding confidentiality and integrity.
-                    </p>
-
-                    <p>
-                        I am alse excited to explore topics like distributed storage and GPU-accelerated training, which are becoming increasingly vital as we continue to develop larger models in areas such as computer vision (CV) and natural language processing (NLP), among others. 
-                        To support these models, we require GPU clusters to accelerate training time and distributed storage to house vast amounts of training data.
-                    </p>
-
-                    <p>
-                        I have experience in artificial intelligence (AI) research and am eager to combine this expertise with system research, such as utilizing AI for SYS. 
-                        I am aware that some AI technologies have already been applied to the testing arena, such as reinforcement learning-based fuzzers. 
-                        However, I believe that AI can play a crucial role in system research due to the many predicting-related situations, such as cache and memory swapping.
-                    </p>
-                </div>
-
-                <div id="Reviewer Services">
-                    <h2>Reviewer Services</h2>
-                    <ul>
-                        <li>Knowledge Discovery and Data Mining (KDD), 2023</li>
-                        <li>International Conference on Machine Learning (ICML) 2023</li>
-                        <li>International Joint Conferences on Artificial Intelligence (IJCAI), 2023</li>
-                    </ul>
                 </div>
 
                 <div id="Projects">
